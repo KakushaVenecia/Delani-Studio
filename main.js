6b24b10d6203c0ed35e486a1d97c1c92cf066ac9
KakushaVenecia <kakushavenecia@gmail.com>
2022-03-14 17:42:42 +0300

    changes made to index, javascript and style pages completed

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-// {/* <div class="row" id="we_do"> */}
- // <img src="Images/design_icon.png" id="des"> 
-// <h5>DESIGN</h5>
- // <p id="design-content" class="hidden-content">
-//     Our design practice offers a full range of services including brand strategy, interaction
-//     and visual design and user experience testing.
-//     Throughout your project, our designers create and implement visual design and workflows,
-//     solicit user feedback and work with you to make sure what gets built is what is needed.
-// </p>
+$('.image').click(function (event) {
+    let container = $(event.target.parent()).attr("id")
+    $(container +"> p").toggleClass("hidden-content")
+})
 
-$(document).onclick(function() {
-    $("hidden-content").click(function() {
-      $("design-content").toggle();
-      $("hidden-content").toggle();
-    });
-  });
-
-  document.getElementById('p')
